2ab16cc5e1927db30fa9f53d08fb0fe88ff91f42
Tom <13624619+meesont@users.noreply.github.com>
2019-03-04 11:22:10 +0000

    Add some number and algorithm solutions

--- Solutions/Numbers/CoinFlipSimulation.py
+++ Solutions/Numbers/CoinFlipSimulation.py
@@ -1,1 +1,23 @@
+import random as r
 numTimes = int(input('Enter the number of times to flip a coin: '))
+
+heads, tails = 0, 0
+
+def flipCoin():
+    choice = r.choice([True, False])
+    # print(choice)
+    if(choice):
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    for i in range(numTimes):
+        if(flipCoin() == True):
+            heads += 1
+        else:
+            tails += 1
+
+    print(f'Heads - {heads}')
+    print(f'Tails - {tails}')
